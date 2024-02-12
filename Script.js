95b66ae142bfe9c55d14bc2d36bc173284164861
AriannaPolla <122610525+AriannaPolla@users.noreply.github.com>
2024-02-12 22:10:24 +0100

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,9 @@
+document.querySelector('.burger-menu').addEventListener('click', function() {
+    document.querySelector('.menu').classList.toggle('active');
+});
 
+var divisor = document.getElementById("divisor"),
+slider = document.getElementById("slider");
+function moveDivisor() { 
+    divisor.style.width = slider.value+"%";
+}
